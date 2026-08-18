636601db8587faa6e09fa7652b17a56f1aac7392
Vo Thanh Dat <195276773+datanhbo08@users.noreply.github.com>
2026-08-18 15:24:42 +0700

    Time: 0 ms (100%), Space: 12.5 MB (21.43%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def largestInteger(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        if k == n: return max(nums)
+
+        fm = defaultdict(int)
+        for num in nums: fm[num] += 1
+
+        unique =[]
+        for num, v in fm.items():
+            if v == 1: unique.append(num)
+        
+        if k == 1: return max(unique) if unique else -1
+        first = nums[0] if fm[nums[0]] == 1 else -1
+        last =  nums[-1] if fm[nums[-1]] == 1 else -1
+
+        return max(first,last)
+
