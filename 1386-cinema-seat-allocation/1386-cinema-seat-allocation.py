059de15f471f7bb224b4c06355ae585d82805589
Vo Thanh Dat <195276773+datanhbo08@users.noreply.github.com>
2026-08-19 22:23:35 +0700

    Time: 135 ms (5.14%), Space: 23.7 MB (7.97%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxNumberOfFamilies(self, n, reservedSeats):
+        """
+        :type n: int
+        :type reservedSeats: List[List[int]]
+        :rtype: int
+        """
+        res = 0
+        mp = defaultdict(set)
+        for r, c in reservedSeats:
+            mp[r].add(c) 
+
+        seenr = 0
+        for k in mp:
+            seenr += 1
+            valid1 = all(j not in mp[k] for j in range(2, 6))   
+            valid2 = all(j not in mp[k] for j in range(4, 8))   
+            valid3 = all(j not in mp[k] for j in range(6, 10)) 
+
+            res += max(valid2, valid1 + valid3)
+
+        return res + (n - seenr) * 2
