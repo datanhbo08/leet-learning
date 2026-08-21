324a84b14cc2eae59b6af4b6ae7d0d702b274caa
Vo Thanh Dat <195276773+datanhbo08@users.noreply.github.com>
2026-08-21 15:18:46 +0700

    Time: 12 ms (68.29%), Space: 20 MB (32.93%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
@@ -1,38 +1,45 @@
 class Solution:
     def findKthSmallest(self, coins: List[int], k: int) -> int:
+
         coins.sort()
         new_coins = []
         for x in coins:
-            if all(x % y for y in new_coins):
+            if all(x % y != 0 for y in new_coins):
                 new_coins.append(x)
-        coins = new_coins
-
-        n = len(coins)
-        m = 1 << n
-        lcm = [1] * m
         
         left = k
-        right = coins[0] * k 
+        right = new_coins[0] * k 
         
-        for mask in range(1, m):
-            pre_mask = mask & (mask - 1)
-            i = (mask & -mask).bit_length() - 1
-            
-            tmp = lcm[pre_mask] * coins[i] // math.gcd(lcm[pre_mask], coins[i])
-            if tmp <= right:
-                lcm[mask] = tmp
-            else:
-                lcm[mask] = right + 1
 
-        add_lcm = [lcm[mask] for mask in range(1, m) if mask.bit_count() % 2 == 1 and lcm[mask] <= right]
-        sub_lcm = [lcm[mask] for mask in range(1, m) if mask.bit_count() % 2 == 0 and lcm[mask] <= right]
-   
-        def count(x: int) -> int:
-            return sum(x // l for l in add_lcm) - sum(x // l for l in sub_lcm)
+        add_lcm = []
+        sub_lcm = []
+        
+        def dfs(idx: int, current_lcm: int, subset_size: int):
+            if subset_size > 0:
+                if subset_size % 2 == 1:
+                    add_lcm.append(current_lcm)
+                else:
+                    sub_lcm.append(current_lcm)
+            
+            for i in range(idx, len(new_coins)):
+                c = new_coins[i]
+                next_lcm = (current_lcm * c) // math.gcd(current_lcm, c)
+                
+                if next_lcm <= right:
+                    dfs(i + 1, next_lcm, subset_size + 1)
+
+        dfs(0, 1, 0)
 
         while left < right:
             mid = (left + right) // 2
-            if count(mid) >= k:
+            
+            res = 0
+            for l in add_lcm:
+                res += mid // l
+            for l in sub_lcm:
+                res -= mid // l
+                
+            if res >= k:
                 right = mid
             else:
                 left = mid + 1
